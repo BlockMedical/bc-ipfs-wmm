d1edd6c35a735f875ed8d857474771cf871f2a51
Wayne Chung <waynechung@Waynes-MacBook-Air.local>
2026-07-10 13:41:03 -0700

    security: upgrade web3 and remove swarm chain

--- bc-ipfs/src/utils/lib_web3.js
+++ bc-ipfs/src/utils/lib_web3.js
@@ -1,18 +1,18 @@
 /* jshint esversion: 6 */
 
 // run with local Provider
-import Web3 from 'web3';
+import { Web3 } from 'web3';
 
 let local_web3;
 
 // overrides metamask v0.2 for our v1.0 or use a local geth instance
 // Fix due to EIP-1102 (https://github.com/ethereum/EIPs/blob/master/EIPS/eip-1102.md)
 if (window.ethereum) {
-  local_web3 = new Web3(ethereum);
+  local_web3 = new Web3(window.ethereum);
   try {
     /* jshint ignore:start */
     async () => {
-      await ethereum.enable();
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
     };
     /* jshint ignore:end */
   } catch (error) {
@@ -20,7 +20,7 @@ if (window.ethereum) {
   }
 } else {
   // backward compatibility
-  local_web3 = new Web3(Web3.givenProvider || new Web3.providers.WebsocketProvider(CONFIG.ethereum.websocket_provider));
+  local_web3 = new Web3(Web3.givenProvider || CONFIG.ethereum.websocket_provider);
 }
 
 const lib_web3 = local_web3;
